c86e5f221f05cfe438b0ba0b8754e89eef821858
tontechio <ilya@ton.org>
2021-11-03 23:02:57 +0300

    re-submit

---
+++ run_opencl.bat
@@ -0,0 +1,14 @@
+@echo off
+
+for /F "tokens=*" %%I in (run_config.ini) do set %%I
+
+set "LOG="
+if "%USE_LOG%"=="Y" (
+  set "LOG=2>&1 >> %LOG_FILE%"
+)
+
+echo on
+:_minerstart
+tonlib-opencl-cli.exe -V
+tonlib-opencl-cli.exe -v 3 -C %CONFIG_FILE% -e "pminer start %GIVER_ADDR% %MY_ADDR% %GPU_ID% %BOOST_FACTOR%" %LOG%
+goto _minerstart
